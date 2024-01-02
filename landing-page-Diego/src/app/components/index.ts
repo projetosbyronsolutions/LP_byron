2451204ec837c8f0df3816f38d634c6a2bea8316
FQuental <felipedoquental@gmail.com>
2024-01-01 23:16:49 -0300

    limpando components extras

--- landing-page-Diego/src/app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Carousel';
-export * from './Footer';
-export * from './Hero';
-export * from './interfaces';
--- landing-page-Diego/src/app/components/interfaces/image.interface.ts
+++
@@ -1,6 +0,0 @@
-
-export interface IImage {
-    path: string;
-    alt: string;
-    size?: number;
-}
